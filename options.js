527bd45c13965112f26622e9171fc3252eff57f4
Aaron Provis <aaroneus@gmail.com>
2014-02-21 12:21:49 +1030

    Fixed issue with Chrome Storage api.
    Chrome storage API processes Asynchronously therefore all post processing needed to be handled in the callback.

--- options.js
+++ options.js
@@ -2,41 +2,49 @@
 function save_options() {
   var select = document.getElementById("length");
   var length = select.children[select.selectedIndex].value;
-
+  // debugger
   var preferences = {};
   preferences['length'] = length;
-  // try{
-  //   localStorage.setItem("length", length);
-  // } catch(e){
-  //   if(e == QUOTA_EXCEEDED_ERR){
-  //     alert(‘Quota exceeded!’); //data wasn’t successfully saved due to quota exceed so throw an error
-  //   }
-  // }
 
-  // Update status to let user know options were saved.
-  var status = document.getElementById("status");
-  status.innerHTML = "Options Saved.";
-  setTimeout(function() {
-    status.innerHTML = "";
-  }, 750);
+  var accName = 'length';
+  var obj = {};
+  obj[accName] = length;
+
+
+  chrome.storage.sync.set(obj, function(){
+    var status = document.getElementById("status");
+    status.innerHTML = "Options Saved.";
+    setTimeout(function() {
+      status.innerHTML = "";
+    }, 750);
+  });
 }
 
 // Restores select box state to saved value from localStorage.
 function restore_options() {
-  // debugger
-  var selected_value = localStorage.getItem("length");
-  if (!selected_value) {
-    return;
-  }
-  var select = document.getElementById("length");
-  for (var i = 0; i < select.children.length; i++) {
-    var child = select.children[i];
-    if (child.value == selected_value) {
-      child.selected = "true";
-      break;
+  debugger
+  var accName = 'length';
+  var selected_value;
+
+  chrome.storage.sync.get(accName, function(data){
+    var val = data[accName];
+    alert("Retrieved: "+val);
+    selected_value = val;
+
+    if (!selected_value) {
+      alert("selected_value is false");
+      return;
     }
-  }
-  alert("Length value was "+ selected_value);
+    var select = document.getElementById("length");
+    for (var i = 0; i < select.children.length; i++) {
+      var child = select.children[i];
+      if (child.value == selected_value) {
+        child.selected = "true";
+        break;
+      }
+    }
+    alert("Length value was "+ selected_value);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
