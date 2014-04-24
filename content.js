ac3b1b86a817b4f1f34b69c1462e6b5f394a35c5
Aaron Provis <aaroneus@gmail.com>
2014-04-24 10:58:50 +0930

    Working with json database f metric export

--- content.js
+++ content.js
@@ -1,9 +1,23 @@
 
-var lookups = ["KM", "M", "Metre", "Kilometre"];
+// var lookups = ["KM", "M", "Metre", "Kilometre"];
+var lookups = metric_data;
+
+// $.getJSON("metric_data.json", function(json) {
+//     lookups = json;
+//     alert(json);
+// });
+
+$.getScript("data.js", function(){
+    lookups = metric_data;
+    // alert(json);
+});
+
+alert("Lookups: "+lookups.length);
+
 var body = $("body").html();
 
 for (var i=0; i < lookups.length; i++) {
-  var unit = lookups[i];
+  var unit = lookups[i].name;
   var searchTerm = new RegExp("\\b[0-9]+[0-9,-\\s]*\\s?"+unit+"[s]?\\b", "gi");
 
   var replaced = body.replace(searchTerm,
